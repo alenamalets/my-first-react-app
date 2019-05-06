2883f94f60bf427ab055be833b8f58c8c0327f1b
Alena <elen.malets@gmail.com>
2019-05-06 11:14:15 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+    <h1>Hello World!!!</h1>
+    <p>It is awesome=)</p>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
